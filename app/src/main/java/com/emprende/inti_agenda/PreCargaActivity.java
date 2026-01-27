f726961cd234482620b876189644924c4a0d88dd
Lurdes Villafañe <villafaneclurdes@gmail.com>
2026-01-27 18:39:46 -0300

    sql Primer commit

---
+++ app/src/main/java/com/emprende/inti_agenda/PreCargaActivity.java
@@ -0,0 +1,33 @@
+package com.emprende.inti_agenda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PreCargaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pre_carga);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            startActivity(new Intent(PreCargaActivity.this, MainActivity.class));
+            finish();
+            }
+        },3000);
+    }
+}
